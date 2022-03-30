75495d167a07a3248605e05f1d9910e0946eb666
MandaNyAina <andrianaivomanda00@gmail.com>
2022-03-30 12:11:12 +0300

    first publish

---
+++ src/components/ToastButtonComponent/ToastButtonComponent.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ToastService } from '../../services/Toast/Toast.service';
+import { Button } from 'primereact/button';
+
+const toast = new ToastService();
+
+export const ToastButtonComponent = (): React.ReactElement => {
+    const testToast = () => {
+        toast.showSuccess('It works !');
+        console.log('passed here');
+    };
+
+    return (
+        <Button
+            className="p-button-success mt-4"
+            onClick={testToast}
+            label="Toast test !"
+        />
+    );
+};
---
+++ src/services/Toast/Toast.service.ts
@@ -0,0 +1,32 @@
+import { toast, ToastOptions, ToastPosition } from 'react-toastify';
+import { I_ToastService } from './I.toast.service';
+
+export class ToastService implements I_ToastService {
+    private position: ToastPosition = 'top-right';
+    private autoClose = 5000;
+
+    private getConfiguration(): ToastOptions {
+        return {
+            position: this.position,
+            autoClose: this.autoClose,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+        };
+    }
+
+    public showSuccess(message: string) {
+        console.log('here');
+
+        toast.success(message, this.getConfiguration());
+    }
+
+    public showError(message: string) {
+        toast.error(message, this.getConfiguration());
+    }
+
+    public showWarning(message: string) {
+        toast.warn(message, this.getConfiguration());
+    }
+}
